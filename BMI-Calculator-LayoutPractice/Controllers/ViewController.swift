b87020ed8d046096ddb4cd58ae264b894961dc0c
keoki33 <gwong323@yahoo.com>
2021-04-07 13:12:40 +1000

    lesson 120 challenge 3 completed

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -19,6 +19,6 @@ class ViewController: UIViewController {
     }
 
     @IBAction func weightSliderChanged(_ sender: UISlider) {
-        print(sender.value)
+        print(Int(sender.value))
     }
 }
